d0a2205bf6971b76e3c227aed9d0bedefd8321ca
Satyam kumar <58924096+Satyam123kumar@users.noreply.github.com>
2024-08-29 23:29:55 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
     // baseURL: "https://mern-realestate-9eew.onrender.com/api",
-    baseURL: "http://localhost:8800/api",
+    baseURL: "https://homequest-ucjd.onrender.com/api",
     withCredentials: true
 })
 
